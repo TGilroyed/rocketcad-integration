b0d44be143cc19d54c6715669539daed1e2abcd6
TGilroyed <40555536+TGilroyed@users.noreply.github.com>
2020-05-11 17:49:49 -0500

    Update sv_version.lua

--- sv_version.lua
+++ sv_version.lua
@@ -1,25 +1,25 @@
 -- Branding!
-local label = [[ RocketCAD by Modern Solutions ]]
+local label = [[^5RocketCAD by Modern Solutions]]
 
 function GetCurrentVersion()
 	return GetResourceMetadata( GetCurrentResourceName(), "version" )
-end 
+end
 
-PerformHttpRequest( "https://wolfknight98.github.io/wk_wars2x_web/version.txt", function( err, text, headers )
+PerformHttpRequest( "https://therocketcad.com/integration-version.txt", function( err, text, headers )
 	Citizen.Wait( 1000 )
 	print( label )
-	local Version = GetCurrentVersion()
-	
-	if ( text ~= nil ) then 
-		print( "  ||    Current version: " .. Version )
-		print( "  ||    Latest recommended version: " .. text .."\n  ||" )
-		
-		if ( text ~= Version ) then
-			print( "  ||    ^1Your Wraith ARS 2X version is outdated, visit the FiveM forum post to get the latest version.\n^0  \\\\\n" )
+	local currentVersion = GetCurrentVersion()
+
+	if ( text ~= nil ) then
+		print( "Your version: " .. currentVersion )
+		print( "Latest version: " .. text .."\n" )
+
+		if ( text ~= currentVersion ) then
+			print( "^3Your RocketCAD Integration script is out of date. Please update by going to modsol.co/rc-update^0  \n" )
 		else
-			print( "  ||    ^2Wraith ARS 2X is up to date!\n^0  ||\n  \\\\\n" )
+			print( "^2RocketCAD Integration is up to date!^0 \n" )
 		end
-	else 
-		print( "  ||    ^1There was an error getting the latest version information, if the issue persists contact WolfKnight#8586 on Discord.\n^0  ||\n  \\\\\n" )
-	end 
+	else
+		print( "^1There was an error getting the latest version information, if this continues please reach out to us using therocketcad.com/support^0 \\\\n" )
+	end
 end )
